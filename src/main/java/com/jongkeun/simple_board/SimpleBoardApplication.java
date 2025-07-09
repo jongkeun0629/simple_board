742c6a9d183b35136a95cb62ac2346e6fdef0479
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-09 10:54:35 +0900

    first commit

---
+++ src/main/java/com/jongkeun/simple_board/SimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jongkeun.simple_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardApplication.class, args);
+	}
+
+}
